d94dba6d7df5408542ff7cd8a53b4dd40fb0b8ec
Dave Canam <david.canam@gmail.com>
2016-04-17 20:22:42 -0300

    added addMotionPath

--- src/js/play.js
+++ src/js/play.js
@@ -33,12 +33,14 @@ Game.Play.prototype = {
 
     this.elevators = this.game.add.physicsGroup();
     this.elevators.setAll('body.allowGravity', false);
-    // this.elevators.setAll('body.immovable', true);
 
-    // this.powerups = this.game.add.group();
-    // this.powerups.enableBody = true;
+    this.platforms = this.game.add.physicsGroup();
+    // this.platforms.setAll('body.allowGravity', false);
+
     this.powerups = this.game.add.physicsGroup();
     this.powerups.setAll('body.immovalbe', true);
+
+    this.bumpers = this.game.add.group();
 
     this.game.physics.startSystem(Phaser.Physics.ARCADE);
 
@@ -51,9 +53,10 @@ Game.Play.prototype = {
 
     this.map.setTileIndexCallback(3, this.playerDead, this);
 
-    this.map.createFromObjects('objects', 4, this.makeBox(160, 32), 0, true, false, this.elevators);
+    this.map.createFromObjects('objects', 4, this.makeBox(160, 32,'#00ff00'), 0, true, false, this.elevators);
+    this.map.createFromObjects('objects', 5, this.makeBox(160, 32, '#ffff00'), 0, true, false, this.platforms);
     this.map.createFromObjects('objects', 7, 'shapes', 0, true, false, this.powerups);
-
+    
     this.layer = this.map.createLayer('layer1');
     this.layer.resizeWorld();
     this.loadObjects();
@@ -83,15 +86,62 @@ Game.Play.prototype = {
   },
   loadObjects: function() {
     this.elevators.forEach(function(e) {
-      e.anchor.set(0.40, 0);
+      // e.anchor.set(0,0.5);
       e.body.immovable = true;
 
-      // var t = this.game.add.tween(p).to({y: (p.distance*p.direction).toString()}, 400).to({y: (p.distance*p.direction*(-1)).toString()}, 400);
       if (e.distance !== undefined) {
         var t = this.game.add.tween(e).to({y: (e.distance*32*e.direction*(-1)).toString()}, (e.distance*300)).to({y: (e.distance*32*e.direction).toString()}, (e.distance*300));
        t.loop(true).start(); 
       }
     }, this);
+
+    
+    this.platforms.forEach(function(p) {
+      // p.body.velocity.x = p.speed;
+			console.log(p.direction);
+
+			p.addMotionPath = function(motionPath) {
+				this.tweenX = this.game.add.tween(this.body);
+				this.tweenY = this.game.add.tween(this.body);
+
+				for(var i = 0; i < motionPath.length; i++) {
+				console.log(motionPath[i].y);
+					this.tweenX.to({x: motionPath[i].x}, motionPath[i].xSpeed, motionPath[i].xEase);
+					this.tweenY.to({y: motionPath[i].y}, motionPath[i].ySpeed, motionPath[i].yEase);
+				}
+				this.tweenX.loop();
+				this.tweenY.loop();
+			};
+			p.start = function() {
+				this.tweenX.start();
+				this.tweenY.start();
+			};
+
+      p.body.immovable = true;
+
+      p.addMotionPath([
+				{ x: "+100", xSpeed: 2000, xEase: "Linear", y: "+0", ySpeed: 2000, yEase: "Sine.easeIn" },
+				{ x: "-100", xSpeed: 2000, xEase: "Linear", y: "-0", ySpeed: 2000, yEase: "Sine.easeOut" }
+			]);
+			p.start();
+
+      // p.body.velocity.x = parseInt(p.speed*p.direction);
+      // e.anchor.set(0.5);
+      // e.body.immovable = true;
+      //
+      // if (e.distance !== undefined) {
+      //   // var t = this.game.add.tween(e).to({x: (e.distance*32*e.direction*(-1)).toString()}, (e.distance*300)).to({x: (e.distance*32*e.direction).toString()}, (e.distance*300));
+      //  //  var t = this.game.add.tween(e).to({x: -100}, 1500).to({x: 100}, 1500);
+      //  // t.loop(true).start(); 
+      //
+      //   this.game.add.tween(e.body.velocity).to( { x: e.distance*32, y: 0 }, 1500, Phaser.Easing.Elastic.Out ).to( { x: e.distance*32*(-1), y: 0 }, 1500, Phaser.Easing.Elastic.Out).yoyo().loop().start();
+      //
+      //
+      //
+      //
+      // }
+    }, this);
+
 
     this.powerups.forEach(function(p) {
       p.frame = parseInt(p.index);
@@ -108,6 +158,13 @@ Game.Play.prototype = {
 
     this.game.physics.arcade.collide(this.player, this.layer);
     this.game.physics.arcade.collide(this.player, this.elevators);
+    // this.game.physics.arcade.collide(this.player, this.platforms, this.setFriction, null, this);
+    this.game.physics.arcade.collide(this.player, this.platforms);
+    //
+    // this.game.physics.arcade.overlap(this.platforms, this.bumpers, this.bouncePlatform, null, this);
+    // this.game.physics.arcade.collide(this.platforms, this.layer, this.bouncePlatform, null, this);
+
+
     this.game.physics.arcade.overlap(this.player, this.powerups, this.pickupPowerup, null, this);
 
     this.player.movements();
@@ -116,6 +173,21 @@ Game.Play.prototype = {
     // // Toggle Music
     // muteKey.onDown.add(this.toggleMute, this);
   },
+  // setFriction: function(player, platform) {
+  //   // player.body.x -= platform.body.x - platform.body.prev.x; 
+  //   player.body.x = player.body.x + (platform.body.x - player.body.x); 
+  // },
+  bouncePlatform: function(platform, bumper) {
+    console.log(platform.direction);
+    if (platform.direction < 0) {
+      platform.body.velocity.x = platform.speed;
+    }else {
+      platform.body.velocity.x = -platform.speed;
+    }
+    platform.direction = platform.direction * -1;
+    // platform.direction *= -1;
+    // platform.body.velocity.x *= -1;
+  },
   pickupPowerup: function(player, powerup) {
     if (powerup.frame === 1) {
       player.triangleUnlocked = true;
@@ -123,11 +195,12 @@ Game.Play.prototype = {
       powerup.kill();
     }
   },
-  makeBox: function(x,y) {
+  makeBox: function(x,y, color) {
     var bmd = this.game.add.bitmapData(x, y);
     bmd.ctx.beginPath();
     bmd.ctx.rect(0, 0, x, y);
-    bmd.ctx.fillStyle = '#00ff00';
+    bmd.ctx.fillStyle = color;
+    // bmd.ctx.fillStyle = '#00ff00';
     bmd.ctx.fill();
     return bmd;
   },
